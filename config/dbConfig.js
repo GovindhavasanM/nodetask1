0a03539b135825345e359040b0b2f6fbe688d04c
GovindhavasanM <santhoshmanigovi@gmail.com>
2024-12-07 00:31:35 +0530

    updated atlas

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/RecipeManager");
+    await mongoose.connect("mongodb+srv://MSanthosh:DlZ6I7SPotpFZ9Tr@cluster0.se47z.mongodb.net/RecipeManager");
     console.log("Database connected");
   } catch (error) {
     console.log("Database connection failed");
